1349de8e5077be0b0b2482ab0fd7b14a9c4217c9
Chris <hakizachrist1@gmail.com>
2023-01-23 17:08:22 +0200

    Update display.js

---
+++ modules/display.js
@@ -0,0 +1,30 @@
+export default function display() {
+  const sectionList = document.getElementById('displaybook');
+  const sectionForm = document.getElementById('form');
+  const sectionContact = document.getElementById('contact');
+  const stayword = document.querySelector('.stay');
+
+  const List = document.getElementById('navList');
+  List.addEventListener('click', () => {
+    sectionList.style.display = 'block';
+    sectionForm.style.display = 'none';
+    sectionContact.style.display = 'none';
+    stayword.style.display = 'none';
+  });
+
+  const Form = document.getElementById('navNew');
+  Form.addEventListener('click', () => {
+    sectionList.style.display = 'none';
+    sectionForm.style.display = 'block';
+    sectionContact.style.display = 'none';
+    stayword.style.display = 'none';
+  });
+
+  const Contact = document.getElementById('navContact');
+  Contact.addEventListener('click', () => {
+    sectionList.style.display = 'none';
+    sectionForm.style.display = 'none';
+    sectionContact.style.display = 'block';
+    stayword.style.display = 'none';
+  });
+}
