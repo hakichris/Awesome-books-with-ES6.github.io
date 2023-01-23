6d17b7a4ec821b7660c062c7e3f71305cecc8744
Chris <hakizachrist1@gmail.com>
2023-01-24 00:16:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 import Book from './modules/Book.js';
 import { DateTime } from './modules/luxon.js';
 import display from './modules/display.js';
@@ -7,11 +6,17 @@ class Library {
   constructor() {
     this.data = [];
   }
-
+  load() {
+    this.data = JSON.parse(localStorage.getItem('library' || '[]'));
+    if (this.data === null) {
+      this.data = [];
+    }
+    this.data.forEach((book) => this.addToUI(book));
+  }
   addBook = (book) => {
     this.data.push(book);
     localStorage.setItem('library', JSON.stringify(this.data));
-    addToUI(book);
+    this.addToUI(book);
   };
 
   removeBook = (id) => {
@@ -20,29 +25,29 @@ class Library {
     this.data = this.data.filter((bookObj) => bookObj.id !== id);
     localStorage.setItem('library', JSON.stringify(this.data));
   };
+
+  addToUI = (bookObj) => {
+    let colorClass = '';
+    if (this.data.indexOf(bookObj) % 2 !== 0) {
+      colorClass = 'light';
+    } else {
+      colorClass = 'dark';
+    }
+    const bookList = document.getElementById('book-list');
+    const book = document.createElement('li');
+    book.classList.add('book');
+    book.classList.add(colorClass);
+    book.setAttribute('id', bookObj.id);
+    book.innerHTML = `<p><span>"${bookObj.title}"</span> by ${bookObj.author}</p>`;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'Remove';
+    deleteBtn.addEventListener('click', () => this.removeBook(bookObj.id));
+    book.appendChild(deleteBtn);
+    bookList.appendChild(book);
+  };
 }
 
 const library = new Library();
-
-const addToUI = (bookObj) => {
-  let colorClass = '';
-  if (library.data.indexOf(bookObj) % 2 !== 0) {
-    colorClass = 'light';
-  } else {
-    colorClass = 'dark';
-  }
-  const bookList = document.getElementById('book-list');
-  const book = document.createElement('li');
-  book.classList.add('book');
-  book.classList.add(colorClass);
-  book.setAttribute('id', bookObj.id);
-  book.innerHTML = `<p><span>"${bookObj.title}"</span> by ${bookObj.author}</p>`;
-  const deleteBtn = document.createElement('button');
-  deleteBtn.innerHTML = 'Remove';
-  deleteBtn.addEventListener('click', () => library.removeBook(bookObj.id));
-  book.appendChild(deleteBtn);
-  bookList.appendChild(book);
-};
 
 const getInput = () => {
   const title = document.getElementById('bookTitle');
@@ -61,11 +66,7 @@ addButton.addEventListener('click', () => {
 
 // Load page
 window.onload = () => {
-  library.data = JSON.parse(localStorage.getItem('library' || '[]'));
-  if (library.data === null) {
-    library.data = [];
-  }
-  library.data.forEach((book) => addToUI(book));
+  library.load();
   display();
 };
 
